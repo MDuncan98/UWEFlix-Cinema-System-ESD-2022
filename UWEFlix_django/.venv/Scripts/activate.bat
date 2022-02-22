eca33d8a62fafb94f47011c67a7d31c802718ae6
rossdwill <47543139+rossdwill@users.noreply.github.com>
2022-02-22 19:09:59 +0000

    testing venv stuff

--- UWEFlix_django/.venv/Scripts/activate.bat
+++ UWEFlix_django/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Micha\OneDrive\Documents\GitHub\Group-18-ESD-2022\UWEFlix_django\.venv
+set VIRTUAL_ENV=E:\Work\ESD\Group -18\Group-18-ESD-2022\UWEFlix_django\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
