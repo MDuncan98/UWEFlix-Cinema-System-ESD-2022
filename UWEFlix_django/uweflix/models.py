7c3453c176f18cc4ab47a74bb6d63952425805c4
MDuncan98 <michael.duncan98@outlook.com>
2022-03-08 13:14:11 +0000

    G18-51: Create Account Models - Account Manager
    I have added the Account model (now called Transaction as to not be mistaken with the existing Account model), which allows the Account Manager to view all existing transactions.
    I have now split this model into two (Transaction and Booking), in order to simplify the transactions page for the the Account Manager, by seperating the individual tickets from the overall transaction itself (especially useful when considering block bookings with a lot of tickets in one transaction).

--- UWEFlix_django/uweflix/models.py
+++ UWEFlix_django/uweflix/models.py
@@ -1,13 +1,21 @@
 from django.db import models
 
-class Account(models.Model):
+class Account(models.Model):  # Database for storing user account information
     username = models.CharField(max_length=100)
     password = models.CharField(max_length=50)
     account_type = models.CharField(max_length=2)
     """Account types:
         - cm = Cinema Manager
         - am = Accounts Manager
-        - cr = Club Representative"""
+        - cr = Club Representative
+        - st = Student"""
+
+class Transaction(models.Model):  # Database for storing all of the 'accounts' to be analysed by Account Manager
+    # transaction_id = order/reference number
+    account = models.ForeignKey(Account, default=1, on_delete=models.SET_DEFAULT)  # User responsible for the transaction
+    date = models.DateField()  # Date of transaction
+    cost = models.FloatField()  # Cost of transaction
+    is_settled = models.BooleanField()  # Whether the transaction has been paid
 
 class Film(models.Model):
     title = models.CharField(max_length=100)
@@ -25,15 +33,22 @@ class Film(models.Model):
 
     def __str__(self):
         return self.title
-   
+    
+class Booking(models.Model):  # Individual ticket booking database
+    transaction = models.ForeignKey(Transaction, default=1, on_delete=models.SET_DEFAULT)
+    # screen = models.ForeignKey(Screen, default=1, on_delete=models.SET_DEFAULT)  # Screen the booking is being viewed at
+    seat_number = models.CharField(max_length=3) # E.G. 1A, 10B, etc.
+    date_time = models.DateTimeField()  # Date and Time of the showing
+    film = models.ForeignKey(Film, default=1, on_delete=models.SET_DEFAULT)
 
 
-class Customer(models.Model):
+class Customer(models.Model):  # Student accounts
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     dob = models.DateField('Date of birth')
-    email = models.EmailField()
-    password = models.CharField(max_length=50)
+    account = models.ForeignKey(Account, default=00000000, on_delete=models.SET_DEFAULT)
+    #email = models.EmailField()
+    #password = models.CharField(max_length=50)
 
 class ClubRep(Customer):
     club_rep_num = models.CharField(max_length=50)
